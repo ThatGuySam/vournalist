b610cf7780ed7960717001f7d62a0fc236cbbefc
Sam Carlton <sam@sam.lc>
2022-06-28 16:05:16 -0500

    Add site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://vournalist.com',
 	adapter: cloudflare(),
 	integrations: [
 		// preact(), 

99c6c6b8541c224b3ddf78e8e38aaebae3b9a109
Sam Carlton <sam@sam.lc>
2022-06-29 12:20:07 -0500

    Configure Astro for Netlify functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config'
+import netlify from '@astrojs/netlify/functions'
 // import preact from '@astrojs/preact'
-// import worker, { cloudflare } from 'astro-service-worker/adapter'
-import cloudflare from '@astrojs/cloudflare'
+// import worker, { cloudflare } from 'astro-service-worker/adapter/index.js'
+// import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://vournalist.com',
-	adapter: cloudflare(),
 	integrations: [
-		// preact(), 
-		// worker(cloudflare)
+		netlify({
+            dist: new URL('./dist/', import.meta.url)
+        }),
 	],
 })
